a46a0c4759987784344289f0ab17a5073de9ae0b
Kevin <kevinsevinche@gmail.com>
2021-09-21 12:59:51 -0500

    Add queries script

--- queries.sql
+++ queries.sql
@@ -8,3 +8,20 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+
+/*
+Write queries to answer the following questions:
+  How many animals are there?
+  How many animals have never tried to escape?
+  What is the average weight of animals?
+  Who escapes the most, neutered or not neutered animals?
+  What is the minimum and maximum weight of each type of animal?
+  What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+*/
+SELECT COUNT(*) AS animals_count FROM animals;
+SELECT COUNT(*) AS animals_tried_escape FROM animals WHERE escape_attempts > 0;
+SELECT AVG(weight_kg) AS average_weight FROM animals;
+SELECT neutered, SUM(escape_attempts) AS escape_count FROM animals GROUP BY neutered;
+SELECT species, MIN(weight_kg), MAX(weight_kg)  FROM animals GROUP BY species;
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '01-01-1990' AND '12-31-2000' GROUP BY species;
