f61fc6b9c1e30b09daafe1ba2d558905c898d5e7
Kevin <kevinsevinche@gmail.com>
2021-09-23 14:14:24 -0500

    Update queries script

--- schema.sql
+++ schema.sql
@@ -50,3 +50,30 @@ ALTER TABLE animals
 ADD CONSTRAINT fk_owners
 FOREIGN KEY (owner_id)
 REFERENCES owners(id);
+
+
+
+
+--## Vet clinic database: add "join table" for visits
+create table vets(
+	id int GENERATED ALWAYS AS IDENTITY,
+	name varchar(100),
+	age int,
+	date_of_graduation date,
+	primary key(id)
+);
+
+create table specializations(
+	vets_id int,
+	species_id int,
+	foreign key (vets_id) references vets(id),
+	foreign key (species_id) references species(id)
+);
+
+create table visits(
+	vets_id int,
+	animals_id int,
+	visit_date date,
+	foreign key (vets_id) references vets(id),
+	foreign key (animals_id) references animals(id)	
+);
